86fe34a287894f6e5596bb929ac86197ffaab2c1
Wen Jiang <abeljiang@hotmail.com>
2019-03-13 02:06:27 -0700

    Start using Mongoose
    Former-commit-id: f26b89e8ba7fad0002f8723ef072f58ac4576176 [formerly 90ffba7dffc6b3fa17cd748e58e0adf68a96e40a] [formerly 310462fdb1f7bfd3e036404cf37008d9b94f1f61 [formerly f78fa6f96ac6d6e9eabcf44fa4ca2bbfd616820c]]
    Former-commit-id: d5a6f7a1ccb0044e0140fbd17402688c82666c9c [formerly f1be59ea80bc8bb178bcda90af6c079c492c1ce5]
    Former-commit-id: 84998146ee0ad02f5b56fc926c7e217e9e5f38ce

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const path = require('path')
 const ereact = require('express-react-views')
+const mongoose = require('mongoose')
 
 const adminRouter = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/error')
-// const sequelize = require('./util/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-// const Order = require('./models/order')
-// const OrderItem = require('./models/order-item')
-// const Cart = require('./models/cart')
-// const CartItem = require('./models/cart-item')
-const mongoConnect = require('./util/database').mongoConnect
-const getDB = require('./util/database').getDB
+// const Product = require('./models/product')
+// const User = require('./models/user')
 
 const app = express()
 
@@ -26,17 +20,16 @@ app.engine('jsx', ereact.createEngine())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(async (req, res, next) => {
-	const db = getDB()
 	try {
-		const user = await db.collection('users').findOne({})
-		req.user = new User(
-			user.id,
-			user.username,
-			user.password,
-			user.email,
-			user.role,
-			user.cart
-		)
+		// const user = await db.collection('users').findOne({})
+		// req.user = new User(
+		// 	user.id,
+		// 	user.username,
+		// 	user.password,
+		// 	user.email,
+		// 	user.role,
+		// 	user.cart
+		// )
 		next()
 	} catch (err) {
 		console.log(err)
@@ -50,54 +43,22 @@ app.use('/', shopRoutes)
 //Fallback Routes
 app.use(errorController.getPageNotFound)
 
-mongoConnect(async () => {
-	// const user = new User('Abel', '1234', 'test@test.com', 'Admin')
-	// await user.save()
-	app.listen(3000)
-	console.log('Start Listening!')
-})
+mongoose
+	.connect(
+		'mongodb+srv://ch48h2o:***REMOVED***@image-shop-brnyc.mongodb.net/test?retryWrites=true',
+		{ useNewUrlParser: true }
+	)
+	.then(result => {
+		console.log('Connected!')
+		app.listen(3000)
+	})
+	.catch(err => {
+		console.log(err)
+	})
 
-// User.hasMany(Product)
-
-// //Create when user create
-// User.hasOne(Cart)
-// Cart.belongsToMany(Product, { as: 'items', through: CartItem })
-// Product.belongsToMany(Cart, { as: 'buyers', through: CartItem })
-
-// //Create when checkout
-// User.hasMany(Order)
-// Order.belongsToMany(Product, { as: 'items', through: OrderItem })
-// Product.belongsToMany(Order, { as: 'orders', through: OrderItem })
-
-// sequelize
-// 	.sync()
-// 	.then(() => {
-// 		process()
-// 	})
-// 	.then(() => {
-// 		app.listen(3000)
-// 	})
-// 	.catch(err => {
-// 		console.log(err)
-// 	})
-
-// const process = async () => {
-// 	try {
-// 		const users = await User.findAll()
-// 		let user
-// 		if (users.length < 1) {
-// 			user = await User.create({
-// 				id: Date.now().toString(),
-// 				name: 'test',
-// 				email: 'test@test.com',
-// 				password: '1234'
-// 			})
-// 			const cart = await Cart.create({ id: Date.now().toString() })
-// 			await user.setCart(cart)
-// 		} else {
-// 			user = users[0]
-// 		}
-// 	} catch (err) {
-// 		console.log(err)
-// 	}
-// }
+// mongoConnect(async () => {
+// 	// const user = new User('Abel', '1234', 'test@test.com', 'Admin')
+// 	// await user.save()
+// 	app.listen(3000)
+// 	console.log('Start Listening!')
+// })
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const shopController = require('../controllers/shop')
+// const shopController = require('../controllers/shop')
 
 const router = express.Router()
 
-router.get('/', shopController.getIndex)
-router.get('/cart', shopController.getCart)
-router.post('/cart', shopController.postCart)
+// router.get('/', shopController.getIndex)
+// router.get('/cart', shopController.getCart)
+// router.post('/cart', shopController.postCart)
 // router.get('/checkout/', shopController.getCheckout)
-router.get('/product-list/:productId', shopController.getDetail)
-router.get('/delete-from-cart/:productId', shopController.getDeleteFromCart)
-router.get('/product-list', shopController.getProducts)
-router.get('/orders', shopController.getOrders)
-router.get('/place-order', shopController.placeOrder)
+// router.get('/product-list/:productId', shopController.getDetail)
+// router.get('/delete-from-cart/:productId', shopController.getDeleteFromCart)
+// router.get('/product-list', shopController.getProducts)
+// router.get('/orders', shopController.getOrders)
+// router.get('/place-order', shopController.placeOrder)
 
 module.exports = router
